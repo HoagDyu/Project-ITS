d8994b5f72ef713e935fa2be657b7f0e537ef8cf
Huỳnh Trần Đức Duy <kenhuynh60@gmail.com>
2026-06-19 20:52:35 +0700

    setup project folder structure

---
+++ backend/mqtt_service/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MqttServiceConfig(AppConfig):
+    name = 'mqtt_service'
---
+++ backend/temp_upload/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class TempUploadConfig(AppConfig):
+    name = 'temp_upload'
